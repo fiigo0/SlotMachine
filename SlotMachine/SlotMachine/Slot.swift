80ef509dcfa10e6a168889f9deca79e9e25d1e46
Alex Diaz <alexdiazorona@me.com>
2015-04-17 16:09:21 -0500

    Final Project added

---
+++ SlotMachine/SlotMachine/Slot.swift
@@ -0,0 +1,17 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Diaz Orona, Jesus A. on 4/14/15.
+//  Copyright (c) 2015 JADO. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+    
+}
